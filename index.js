2255e681c1d2a1aa652609fbde1d6440a375603e
Derrick Skipwith <dskipwith15@gmail.com>
2018-08-09 04:57:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 function dwarfRollCall(dwarves) {
+  var sentence = ""
+  var i = 0
+  while (i < dwarves.length) {
+    sentence = sentence + `${i + 1}. ${dwarves[i]} `
+    i = i + 1
+  }
+  return sentence
+}
+ function summonCaptainPlanet(planeteerCalls){
+  var array = []
+  var i = 0
+  while (i < planeteerCalls.length) {
+    array.push(`${planeteerCalls[i].toUpperCase()}!`)
+    i++
+  }
+  return array
 }
 
-function summonCaptainPlanet(planeteerCalls){
+ function longPlaneteerCalls(words) {
+  var i = 0
+  while (i < words.length) {
+    if (words[i].length > 4) {
+      return true
+    }
+    else {
+      i++
+    }
+  }
+  return false
 }
 
-function longPlaneteerCalls(words) {
-}
-
-function findTheCheese (foods) {
+ function findTheCheese (foods) {
+  var cheese_array = ["cheddar", "gouda", "camembert"]
+  var i = 0
+  while (i < foods.length) {
+    if (cheese_array.includes(foods[i])) {
+      return foods[i]
+    }
+    else {
+      i++
+    }
+  }
+  return "no cheese!"
 }
